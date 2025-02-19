e92192b04c162f2b830670051a5a5f64968d3b8e
David Navarro <davidnavarrom3@gmail.com>
2025-02-19 21:19:42 +0100

    Add top bar example

---
+++ app/src/main/java/com/composeglass/components/GlassTopAppBarExample.kt
@@ -0,0 +1,28 @@
+package com.composeglass.components
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import com.composeglass.modifier.blurGlass
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun GlassTopAppBarExample(title: String) {
+    TopAppBar(
+        title = {
+            Text(
+                text = title,
+                modifier = Modifier.fillMaxWidth(),
+                textAlign = TextAlign.Center
+            )
+        },
+        modifier = Modifier
+            .fillMaxWidth()
+            .blurGlass(blurRadius = 20, blurOpacity = 0.5f, blurColor = Color.White)
+    )
+}
