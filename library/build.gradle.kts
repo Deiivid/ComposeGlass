03dce8826273c0232b00de8d39e02955b32176cb
dnavarro <dnavarro@hiberus.com>
2025-03-03 12:32:53 +0100

    Minor- fix dependencie

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -46,6 +46,6 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-
+    implementation("androidx.compose.foundation:foundation-layout")
 }
 
