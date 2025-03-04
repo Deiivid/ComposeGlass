85ac7d3a88cb33b2bc52675140af09157732517d
dnavarro <dnavarro@hiberus.com>
2025-03-04 12:36:25 +0100

    Minor- add maven publish & fix testing

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -39,7 +39,18 @@ android {
         compose = true
     }
 }
-
+afterEvaluate {
+    publishing {
+        publications {
+            create<MavenPublication>("release") {
+                from(components["release"])
+                groupId = "com.github.Deiivid"
+                artifactId = "ComposeGlass"
+                version = "1.0.0"
+            }
+        }
+    }
+}
 
 dependencies {
     implementation(libs.androidx.core.ktx)
@@ -50,4 +61,11 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.ui.test.junit4)
+    debugImplementation(libs.androidx.ui.tooling)
+    debugImplementation(libs.androidx.ui.test.manifest)
 }
