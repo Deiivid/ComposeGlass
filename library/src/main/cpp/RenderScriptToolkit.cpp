c6e37f61f2d146c6038c6bcdc3b06c9982123949
dnavarro <dnavarro@hiberus.com>
2025-03-24 10:53:15 +0100

    Minor- Add new RenderScriptToolkit.cpp

--- library/src/main/cpp/RenderScriptToolkit.cpp
+++ library/src/main/cpp/RenderScriptToolkit.cpp
@@ -1,4 +1,6 @@
 #include "RenderScriptToolkit.h"
+#include "blur.h"
+#include <cstring>
 
 namespace composeglass {
 
@@ -11,16 +13,9 @@ namespace composeglass {
         size_t startY = restriction ? restriction->startY : 0;
         size_t endY = restriction ? restriction->endY : height;
 
-        // Aquí pon tu algoritmo real. De momento copiamos tal cual.
-        for (size_t y = startY; y < endY; y++) {
-            for (size_t x = startX; x < endX; x++) {
-                size_t idx = y * stride + x * 4;
-                output[idx + 0] = input[idx + 0];
-                output[idx + 1] = input[idx + 1];
-                output[idx + 2] = input[idx + 2];
-                output[idx + 3] = input[idx + 3];
-            }
-        }
+        // Por ahora, no se usa la restricción
+        std::memcpy(output, input, height * stride);
+        stackBlur(output, width, height, radius); // <- aquí está la llamada a tu función nativa
     }
 
-} // namespace composeglass
+}ya
